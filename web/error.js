803ef74dbece58d572d00c07d4ee1e36a1288a5f
keel <keel.sike@gmail.com>
2016-09-15 20:32:55 +0800

    完成kc init指令

--- web/error.js
+++ web/error.js
@@ -1,11 +1,12 @@
 /*
 通用的错误码,根据情况不断追加
-author:Keel
  */
 'use strict';
 var kc = require('../lib/kc');
 var error = kc.error;
 var err = error.err;
+
+
 err['testErr'] = '404004';
 
 exports.err = err;
