95f311abf9b9ef5d03f06853b87ae9b088037b23
keel <keel.sike@gmail.com>
2016-09-15 21:33:03 +0800

    调整kc指令init文件列表

--- tpl_kc.js
+++ tpl_kc.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var kc = require('./lib/kc');
-var app = kc.createApp(__dirname, { 'isRedisInit': true, 'isMongoInit': true });
+var app = kc.createApp(__dirname, { 'isRedisInit': false, 'isMongoInit': false });
 app.get('/logout', kc.sessionAuth.logout);
 app.start([#port]);
