48f9472f3fd3b3e33def761aa89fae996e187334
keel <keel.sike@gmail.com>
2016-09-21 13:18:55 +0800

    增加mysql,调整配置文件结构

--- kc-.js
+++ kc-.js
@@ -7,7 +7,8 @@ var kconfig = require('ktool').kconfig;
 kconfig.init(path.join(__dirname, 'config.json'));
 
 //生成项目express主进程
-var app = kc.createApp(__dirname, { 'isRedisInit': false, 'isMongoInit': false });
+// var app = kc.createApp(__dirname, { 'configFile': path.join(__dirname, 'config.json') });
+var app = kc.createApp(__dirname);
 
 
 //增加非api和tpl的路由,如logout,此处为express的标准用法
--- kc-1.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var kc = require('./lib/kc');
-var app = kc.createApp(__dirname, { 'isRedisInit': true, 'isMongoInit': true });
-app.get('/logout', kc.sessionAuth.logout);
-app.start(15001);
