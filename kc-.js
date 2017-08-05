190a53332c08dd9228e002834a08e83356042f1b
keel <keel.sike@gmail.com>
2017-08-05 23:24:50 +0800

    db类可同时加载多个配置

--- kc-.js
+++ kc-.js
@@ -3,19 +3,20 @@
 const kc = require('./lib/kc');
 const path = require('path');
 const kconfig = require('ktool').kconfig;
+const vlog = require('vlog').instance(__filename);
 
 
 //生成项目express主进程
-// const app = kc.createApp(__dirname, { 'configFile': path.join(__dirname, 'config.json') });
+// const app = kc.createApp(__dirname);
 const app = kc.createApp(__dirname);
 
 //附加有dbsInitOK事件会在redis,mongo.mysql初始化完成后触发
 // app.on('dbsInitOK',function(err) {
 //   if (err) {
-//     console.error(err);
+//     vlog.eo(err, 'init:dbsInit');
 //     return;
 //   }
-//   console.log('init Ok!!');
+//   vlog.log('init Ok!!');
 // });
 
 //增加非api和tpl的路由,如logout,此处为express的标准用法
